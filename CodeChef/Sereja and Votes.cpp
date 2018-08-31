05388bed07b7d5497449c52157cf5b9ec0f57440
dhruvik <navadiyadhruvik2@gmail.com>
2018-08-31 00:34:07 -0700

    right answer for vote.

--- CodeChef/Sereja and Votes.cpp
+++ CodeChef/Sereja and Votes.cpp
@@ -11,24 +11,20 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
-
     while(t--){
         int n;
         cin >> n;
-        double t,sum;
-
+        long long int t,sum=0,sum1=0;
         for(int i=0;i<n;i++){
             cin >> t;
             sum+=t;
+            if(t>0)
+            sum1+=t-1;
         }
-
-        if(sum>=100){
-            if(sum-100<n){
-                cout << "YES\n";
-            }else{
-                cout <<"NO\n";
-            }
-        }else{
+        if(sum1<100 && 100<=sum){
+                cout <<"YES\n";
+        }
+        else{
             cout << "NO\n";
         }
     }
