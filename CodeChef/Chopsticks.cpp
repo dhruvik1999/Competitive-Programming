8d6e2712b9926aeb50934750c3a036d60d52ba6c
Dhruvik1999 <navadiyadhruvik2@gmail.com>
2018-12-05 21:10:08 -0800

    cs MID

---
+++ CodeChef/Chopsticks.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin >> a[i];
+
+    sort(a,a+n);
+
+    int ans=0;
+
+    for(int i=0;i<n-1;i++){
+        if(a[i+1]-a[i]<=k){
+            ans++;
+            i++;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
