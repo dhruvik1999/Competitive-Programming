ccd8ef0b9a5ca36d83972f464f103da3b2017708
Dhruvik1999 <navadiyadhruvik2@gmail.com>
2018-12-11 11:31:24 -0800

    added all define and typedef for cp

---
+++ Templets.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define mi map<int,int>
+#define mll map<ll,ll>
+#define si set<int>
+#define sll set<ll>
+#define usi unordered_set<int>
+#define usll unordered_set<ll>
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define F first
+#define S second
+
+#define MOD 1000000007
+#define maxlen 1000005
+
+
