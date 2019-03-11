f4baef010813afb01580ced3b786cbd31e588a6e
Dhruvik <navadiyadhruvik2@gmail.com>
2019-03-11 16:15:27 +0530

    temp1 aaded.

---
+++ temp1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+#define fast1 ios_base::sync_with_stdio(false)
+#define fast2 cin.tie(NULL)
+#define ll long long
+
+using namespace std;
+
+int main(){
+
+    fast1;
+    fast2;
+
+
+    return 0;
+}
