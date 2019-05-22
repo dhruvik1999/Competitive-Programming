cbc66d39dd69919237f9150dae1dda4dfbf245e4
Dhruvik Navadiya <navadiyadhruvik2@gmail.com>
2019-05-22 19:51:16 +0530

    is pal for ll

--- GeeksForGeeks/rev.py
+++ GeeksForGeeks/rev.py
@@ -7,14 +7,20 @@ class LL:
 	def __init__(self):
 		self.head = None
 		self.length = 0
+		self.dummy = None
+		self.ans = True
+
 
 	def addNode(self,data):
 		if(self.head == None):
 			self.head = Node(data)
+			self.dummy = self.head
 		else:
 			node = Node(data)
 			node.next=self.head
 			self.head=node
+			self.dummy = self.head
+
 
 	def printLL(self):
 		temp = self.head
@@ -36,6 +42,21 @@ class LL:
 			two=three
 			if three != None:
 				three=three.next
+
+	def isPal(self,node):
+		if node==None:
+			return
+
+		self.isPal(node.next)
+
+		if node!=None and self.dummy != None:
+			print(self.dummy.data , " - " ,node.data)
+			if(node.data == self.dummy.data):
+				self.dummy = self.dummy.next
+			else:
+				self.ans=False
+
+
 		
 
 
@@ -45,7 +66,14 @@ ll.addNode(2)
 ll.addNode(3)
 ll.addNode(4)
 ll.addNode(5)
+ll.addNode(4)
+ll.addNode(3)
+ll.addNode(2)
+ll.addNode(1)
 ll.printLL()
-print("---------------")
-ll.rev()
-ll.printLL()
+print("dummy : ",ll.dummy.data)
+ll.isPal(ll.head)
+if ll.ans:
+	print("is  pal")
+else:
+	print("is not pal")
