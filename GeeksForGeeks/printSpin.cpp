61dcf03c82d8ef953dbee2d76a62c0dbcf351c31
Dhruvik Navadiya <navadiyadhruvik2@gmail.com>
2019-05-24 20:34:37 +0530

    spinner order

--- GeeksForGeeks/printSpin.cpp
+++ GeeksForGeeks/printSpin.cpp
@@ -3,15 +3,45 @@ using namespace std;
 
 int main(){
 
-	int n,m;
-	cin >> n >> m;
+	int n;
+	cin >> n;
 
-	int a[n][m];
+	int a[n][n];
 	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
+		for(int j=0;j<n;j++){
 			cin >> a[i][j];
 		}
 	}
+
+	int r=n-1,l=0,d=n-1,u=0;
+
+	while(l<=r && u<=d){
+		
+		
+		for(int i=l;i<=r;i++){
+			cout << a[u][i] << " ";
+		}
+		u++;
+
+		for(int i=u;i<=d;i++){
+			cout << a[i][r] << " ";
+		}
+		r--;
+
+		for(int i=r;i>=l;i--){
+			cout  << a[d][i] << " ";
+		}
+		d--;
+
+		for(int i=d;i>=u;i--){
+			cout << a[i][l] << " ";
+		}
+		l++;
+	}
+
+	cout << "\n";
+
+
 
 
 
