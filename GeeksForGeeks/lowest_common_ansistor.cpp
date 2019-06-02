928b5a4750a46015ed0daceda829b42f0835f977
Dhruvik Navadiya <navadiyadhruvik2@gmail.com>
2019-06-02 14:58:21 +0530

    lowest common ansistor added

---
+++ GeeksForGeeks/lowest_common_ansistor.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	struct Node *left,*right;
+};
+
+struct Node* newNode(int data){
+	struct Node* node = new Node;
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+}
+
+
+struct Node* root = NULL;
+
+
+void inOrd(struct Node* node){
+	if(!node)
+		return;
+
+	inOrd(node->left);
+	cout << node->data << " ";
+	inOrd(node->right);
+}
+
+void findLCA(){
+	//find path of a and b to root
+	//find the first common node which is lowest common ansistor
+}
+
+
+int main(){
+
+	root = newNode(15);
+	root->right = newNode(20);
+	root->right->right = newNode(25);
+	root->right->left = newNode(16);
+	root->left=newNode(10);
+	root->left->left=newNode(8);
+	root->left->right=newNode(12);
+
+	inOrd(root);
+	cout << "\n";
+
+
+
+	return 0;
+}
