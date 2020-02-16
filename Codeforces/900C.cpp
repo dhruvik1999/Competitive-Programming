e5ff0185e2e734a997084d6e74e5440e1b72a0bc
dhruvik1999 <navadiyadhruvik2@gmail.com>
2020-02-16 09:44:00 +0530

    900C_CF

---
+++ Codeforces/900C.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MOD 1000000007
+#define maxlen 9223372036854775807
+#define minlen -9223372036854775807
+#define fin(n) for(int i=0;i<n;i++)
+
+int gcd(int a,int b){if(b==0)return a;return gcd(b,a%b);}
+vector<int> primeFact(int n){vector<int> ans;for(int t=2;t<=sqrt(n);t++){if(n%t==0){ans.push_back(t);while(n%t==0){n=n/t;}}}if(n>2)ans.push_back(n);return ans;}
+vector<int> fact(int n){vector<int> ans;for(int i=1;i<=sqrt(n);i++){if(n%i==0){if( i*i == n ){ans.push_back(i);}else{ans.push_back(i);ans.push_back(n/i);}}}return ans;}
+int powerr(int base,int exp,int mod) { if(exp==0)return 1;int t = powerr(base,exp/2,mod)%mod;if(exp%2==0){return (t%mod*t%mod)%mod;}else{return (t%mod*t%mod*base%mod)%mod;}} 
+void fastio(){ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+
+
+
+int32_t main(){
+	fastio();
+
+	int n;
+	cin >> n;
+
+	vector<int> a(n);
+	set<int> s;
+	for(int i=0;i<n;i++){
+		cin >> a[i];
+		s.insert(a[i]);
+	}
+
+	priority_queue<int> pq;
+	pq.push(-1);
+
+	int l,ll;
+
+	map<int,int> mp;
+	int mm = 0,ans=-1;
+
+	vector<int> dp(n,0);
+	int mmm=0;
+	for(int i=0;i<n;i++){
+		if( a[i]>mmm ){
+			dp[i]=1;
+			mmm = a[i];
+			mp[a[i]]--;
+		}else{
+			mp[a[i]]=0;
+		}
+	}
+
+
+
+	for(int i=0;i<n;i++){
+		if( pq.top() < a[i] ){
+			pq.push( a[i] );
+		}else{
+			l=pq.top();
+			pq.pop();
+			ll=pq.top();
+
+			if( a[i]>ll ){
+				mp[l]++;
+			}
+
+			pq.push(l);
+			pq.push(a[i]);
+		}
+	}
+
+	mm=minlen;
+	for( auto temp : mp ){
+			//cout << temp.first << " " << temp.second << "\n";
+		if( temp.second > mm || temp.second==mm && temp.first<=ans  ){
+			ans=temp.first;
+			mm=temp.second;
+		}
+	}
+
+	cout << ans << "\n";
+
+
+
+	return 0;
+}
+
+
+
+
