459d805a7f0a5bd6d1f0aaeb39990b9f8fedf302
dhruvik1999 <navadiyadhruvik2@gmail.com>
2020-02-18 13:33:19 +0530

    960_C_CF

---
+++ Codeforces/960C.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MOD 1000000007
+#define maxlen 9223372036854775807
+#define minlen -9223372036854775807
+#define fin(n) for(int i=0;i<n;i++)
+
+int gcd(int a,int b){if(b==0)return a;return gcd(b,a%b);}
+vector<int> primeFact(int n){vector<int> ans;for(int t=2;t<=sqrt(n);t++){if(n%t==0){ans.push_back(t);while(n%t==0){n=n/t;}}}if(n>2)ans.push_back(n);return ans;}
+vector<int> fact(int n){vector<int> ans;for(int i=1;i<=sqrt(n);i++){if(n%i==0){if( i*i == n ){ans.push_back(i);}else{ans.push_back(i);ans.push_back(n/i);}}}return ans;}
+int powerr(int base,int exp,int mod) { if(exp==0)return 1;int t = powerr(base,exp/2,mod)%mod;if(exp%2==0){return (t%mod*t%mod)%mod;}else{return (t%mod*t%mod*base%mod)%mod;}} 
+void fastio(){ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+
+
+
+int32_t main(){
+	fastio();
+
+	int n,x;
+	cin >> n >> x;
+
+	string s = "";
+	int cp =0;
+	for(int i=0;i<63;i++){
+		//cout  << ((n)&((1LL)<<i)) << "\n";
+		if( ((n)&((1LL)<<i))!=0 ){
+			s=s+"1";
+			cp++;
+		}else{
+			s=s+"0";
+		}
+	}
+
+//cout << s << "\n";
+
+	int t=1;
+	vector<int> ans;
+	for(int i=0;i<s.length();i++){
+		if( s[i]=='1' ){
+			for(int j=0;j<i;j++){
+				ans.push_back(t);
+			}
+		t+=x;
+
+		}
+	}
+
+	for(int i=0;i<cp;i++){
+		ans.push_back( t );
+		t+=x;
+	}
+
+	cout << ans.size() << "\n";
+	for(int i=0;i<ans.size();i++){
+		cout << ans[i] << " ";
+	}
+	cout << "\n";
+
+
+
+	return 0;
+}
